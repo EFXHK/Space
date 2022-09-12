166fd6d922df49379a683606ea8b8dd5d0eb1f68
STEEL <royal@STEEL.local>
2022-09-12 20:24:15 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+star_1 = "Vega"
+star_2 = "Ursa Major"
+true_size = 8.61
+a = " is "
+b = " times larger than "
+c = ". This is a wildly inaccurate fact and I forgot the easier way to do this."
+
+print ((star_1) + (a) + str(true_size) + (b) + str(star_2) + (c))
